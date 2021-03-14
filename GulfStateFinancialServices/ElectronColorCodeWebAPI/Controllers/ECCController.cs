5ee3d210e782a765712c0de869ecf67551921e1f
Jasmit Tarang <j_hack92@Jasmits-iMac.local>
2021-03-14 05:45:46 -0500

    A simple web api

---
+++ GulfStateFinancialServices/ElectronColorCodeWebAPI/Controllers/ECCController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using ElectronColorCode;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ElectronColorCodeWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ECCController : Controller
+    {
+        private ECCTable eccTable = new ();
+
+        // GET: api/ECC
+        [HttpGet]
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(eccTable.eccTable.ToArray());
+        }
+
+        // GET api/ECC/green
+        [HttpGet("{eccColor}")]
+        public string Get(string eccColor)
+        { 
+            return JsonConvert.SerializeObject(eccTable.GetElectronColorCode(eccColor));
+        }
+    }
+}
